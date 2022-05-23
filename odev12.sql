2f62c4e9dd79f00bffd3987501372cece40b23b4
Ganc Serdar Menesyan <ganc_menesyan@hotmail.com>
2022-05-23 16:46:06 +0300

    Create odev12.sql

---
+++ odev12.sql
@@ -0,0 +1,12 @@
+1)
+SELECT COUNT(*) FROM film WHERE length > ( SELECT AVG(length) FROM film);
+
+2)
+SELECT COUNT(*) FROM film  WHERE rental_rate = (SELECT MAX(rental_rate) from film);
+
+3)
+SELECT title FROM film WHERE rental_rate = (SELECT MIN(rental_rate) FROM film) AND replacement_cost = (SELECT MIN(replacement_rate) from film)
+
+4)
+SELECT customer FROM payment INNER JOIN customer ON customer.customer_id=payment.customer_id
+GROUP BY customer.customer_id ORDER BY COUNT(payment_id);
