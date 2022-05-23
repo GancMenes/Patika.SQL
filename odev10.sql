08bac5dbb8c62771e4ac7f5bbffacd07dc89cf16
Ganc Serdar Menesyan <ganc_menesyan@hotmail.com>
2022-05-23 11:29:55 +0300

    Update odev10.sql

--- odev10.sql
+++ odev10.sql
@@ -1,1 +1,17 @@
+SELECT city, country 
+FROM city LEFT 
+JOIN country 
+ON country.country_id = city.country_id;
 
+
+SELECT customer.first_name, customer.last_name, payment.payment_id 
+FROM custoemr RIGHT 
+JOIN payment 
+ON customer.customer_id = payment.customer_id;
+
+
+SELECT customer.first_name, customer.last_name, rental.rental_id
+FROM customer
+FULL JOIN rental
+ON customer.cutsomer.customer_id = rental.customer_id
+ORDER BY rental_id;
